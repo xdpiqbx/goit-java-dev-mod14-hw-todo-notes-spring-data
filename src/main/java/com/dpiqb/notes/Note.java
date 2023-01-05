04d20f60fd7f9e5f49c43ed2ea05c7ce6d196662
xdpiqbx <xdpiqbx@gmail.com>
2023-01-05 22:46:29 +0200

    tasks 1, 2, 3 done

--- src/main/java/com/dpiqb/notes/Note.java
+++ src/main/java/com/dpiqb/notes/Note.java
@@ -1,12 +1,16 @@
 package com.dpiqb.notes;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-//@Entity
+@Entity
 public class Note {
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
   private String title;
   private String content;
